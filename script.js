5bdd23b183ad0c1ba224645428000afd299bdc43
MPJM <92897452+MPJM@users.noreply.github.com>
2023-05-26 01:41:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function getCharacters(done) {
+  const results = fetch("https://fakestoreapi.com/products");
+
+  results
+    .then((response) => response.json())
+    .then((data) => {
+      done(data);
+    });
+}
+
+getCharacters((data) => {
+  data.forEach((item) => {
+    const article = document.createRange().createContextualFragment(/*html*/ `
+    <article class="box">
+    <div class="image-container">
+      <img src="${item.image}" alt="${item.category}" id="zoom-image" />
+      <div class="overlay">
+        <div class="overlay-content">
+          Comprar
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h2>${item.title}</h2>
+      <p>${item.description}</p>
+    </div>
+    <p class="price">$${item.price}</p>
+  </article>
+  
+  
+        `);
+
+    const main = document.querySelector("main");
+    main.append(article);
+  });
+});
